20581f438aa230327f94e0ed6af658ea4fc0941c
Gautam <79162790+mgautam07@users.noreply.github.com>
2021-10-08 20:57:39 +0530

    Update data.js

--- src/TribeVibe/components/data.js
+++ src/TribeVibe/components/data.js
@@ -5,8 +5,7 @@ const society = [{
     "img2" : "./TribeVibe/Assets/socieites/Rotract/Rotract-2.jpeg",
     "img3" : "./TribeVibe/Assets/socieites/Rotract/Rotract-3.jpeg",
     "img4" : "./TribeVibe/Assets/socieites/Rotract/Rotract-4.jpeg",
-    "desc" : "Rotaract as an organisation currently function in more than 180+ countries originally began as a Rotary International youth program in 1968 at Charlotte North Rotary Club in Charlotte, North Carolina, United States, and has grown into a major organization of over 10,904 clubs spread around the world and over 203,000 members and are serving the society with the motto, Self Development | Fellowship through Service.
-Here at Rotaract you don't only get the chance to become an exemplary leader but also an emotionally intellectual human being who takes on world problems with utmost care and precision to benefit society in the end."
+    "desc" : "Rotaract as an organisation currently function in more than 180+ countries originally began as a Rotary International youth program in 1968 at Charlotte North Rotary Club in Charlotte, North Carolina, United States, and has grown into a major organization of over 10,904 clubs spread around the world and over 203,000 members and are serving the society with the motto, Self Development | Fellowship through Service.Here at Rotaract you don't only get the chance to become an exemplary leader but also an emotionally intellectual human being who takes on world problems with utmost care and precision to benefit society in the end."
 },
 {
     key:2,
@@ -14,8 +13,7 @@ Here at Rotaract you don't only get the chance to become an exemplary leader but
     "img1" : "./TribeVibe/Assets/socieites/mudra/mudra-1.jpg",
     "img2" : "./TribeVibe/Assets/socieites/mudra/mudra-2.png",
     "img3" : "./TribeVibe/Assets/socieites/mudra/mudra-3.png",
-    "desc" : "Hey Juniors,
-Get ready for the most exciting event of Tribe Vibe. Mudra Society invites you to Razzle Dazzle, a fun-filled event with loads of games and many rewards. We indeed have something in store for all of you. Our team knows you all are stressed about your college life. Thus, MUDRA remains true to its vision and comes to rescue you out of tedious coding and studies. "
+    "desc" : "Hey Juniors, Get ready for the most exciting event of Tribe Vibe. Mudra Society invites you to Razzle Dazzle, a fun-filled event with loads of games and many rewards. We indeed have something in store for all of you. Our team knows you all are stressed about your college life. Thus, MUDRA remains true to its vision and comes to rescue you out of tedious coding and studies. "
 },
 {
     key:3,
@@ -23,22 +21,15 @@ Get ready for the most exciting event of Tribe Vibe. Mudra Society invites you t
     "img1" : "./TribeVibe/Assets/socieites/mlsc/mlsc-1.jpg",
     "img2" : "./TribeVibe/Assets/socieites/mlsc/mlsc-2.jpg",
     "img3" : "./TribeVibe/Assets/socieites/mlsc/mlsc-3.jpg",
-    "desc" : "Microsoft Learn Student Chapter is a technical society devoted to elevating the coding culture of Thapar Institute of Engineering and Technology by mentoring and motivating its students to refine their critical thinking, logical reasoning, and coding skills.
-
-MLSC was formed with the motive of providing the students a dynamic platform that serves as nodes of activity for not only its members but the computing community at large, offering all possible resources and opportunities to learn, master, and polish their abilities. 
-
-We value holistic growth which includes injecting soft skills along with cultivating a  progressive mindset. MLSC effectuates it by organizing numerous technology-driven events including seminars, lectures, and the opportunity to meet peers and experts in many fields of interest, tech meetups, and Hackathons, and promoting team building. 
-
-
-In the long run, MLSC plans to foster nurture young minds of TIET to a magnitude where they are proficient enough to compete amongst the nonpareil, thus upheaving the name of TIET."
+    "desc" : "Microsoft Learn Student Chapter is a technical society devoted to elevating the coding culture of Thapar Institute of Engineering and Technology by mentoring and motivating its students to refine their critical thinking, logical reasoning, and coding skills. MLSC was formed with the motive of providing the students a dynamic platform that serves as nodes of activity for not only its members but the computing community at large, offering all possible resources and opportunities to learn, master, and polish their abilities. We value holistic growth which includes injecting soft skills along with cultivating a  progressive mindset. MLSC effectuates it by organizing numerous technology-driven events including seminars, lectures, and the opportunity to meet peers and experts in many fields of interest, tech meetups, and Hackathons, and promoting team building. In the long run, MLSC plans to foster nurture young minds of TIET to a magnitude where they are proficient enough to compete amongst the nonpareil, thus upheaving the name of TIET."
 },
 {
     key:5,
-    "name" : "backslash",
-    "img1" : "Screenshot.png",
-    "img2" : "Screenshot.png",
-    "img3" : "Screenshot.png",
-    "desc" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar eros arcu, id aliquet lacus vulputate dapibus. Morbi felis massa, faucibus vel sapien non, vulputate convallis eros. Nunc mi arcu, fringilla at nulla eget."
+    "name" : "FAPS",
+    "img1" : "./TribeVibe/Assets/socieites/faps/faps-1.jpg",
+    "img2" : "./TribeVibe/Assets/socieites/faps/faps-1.jpg",
+    "img3" : "./TribeVibe/Assets/socieites/faps/faps-1.jpg",
+    "desc" : "College Life is indeed incomplete without pursuing one's passion. FAPS Derabassi is here to fix this never-ending problem. We are a team of skilled and creative individuals who strive to bring exceptional opportunities to everyone around us. FAPS Derabassi is a Citadel for Fine Arts, Photography, and Design to flourish. It is the perfect platform to showcase your talent and grow as an Individual. We aim to create a community of like-minded artists who love to express their emotions through their Artwork."
 }
 ]
 
