ed912283347d1e25d96e1a128cd476eeebae3aaf
Gautam <79162790+mgautam07@users.noreply.github.com>
2021-10-08 19:03:58 +0530

    Update data.js

--- src/TribeVibe/components/data.js
+++ src/TribeVibe/components/data.js
@@ -1,11 +1,12 @@
 const society = [{
     key:1,
-    "name" : "backslash",
+    "name" : "Rotract",
     "img1" : "./TribeVibe/Assets/socieites/Rotract/Rotract-1.jpeg",
     "img2" : "./TribeVibe/Assets/socieites/Rotract/Rotract-2.jpeg",
     "img3" : "./TribeVibe/Assets/socieites/Rotract/Rotract-3.jpeg",
     "img4" : "./TribeVibe/Assets/socieites/Rotract/Rotract-4.jpeg",
-    "desc" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar eros arcu, id aliquet lacus vulputate dapibus. Morbi felis massa, faucibus vel sapien non, vulputate convallis eros. Nunc mi arcu, fringilla at nulla eget."
+    "desc" : "LRotaract as an organisation currently function in more than 180+ countries originally began as a Rotary International youth program in 1968 at Charlotte North Rotary Club in Charlotte, North Carolina, United States, and has grown into a major organization of over 10,904 clubs spread around the world and over 203,000 members and are serving the society with the motto, Self Development | Fellowship through Service.
+Here at Rotaract you don't only get the chance to become an exemplary leader but also an emotionally intellectual human being who takes on world problems with utmost care and precision to benefit society in the end."
 },
 {
     "key" : 2,
