efd35861b434e196fba1baca71de26a18a7a5487
Gautam <79162790+mgautam07@users.noreply.github.com>
2021-10-08 19:01:41 +0530

    Updated Rotract images paths in data.js

--- src/TribeVibe/components/data.js
+++ src/TribeVibe/components/data.js
@@ -1,9 +1,10 @@
 const society = [{
     key:1,
     "name" : "backslash",
-    "img1" : "Screenshot.png",
-    "img2" : "Screenshot.png",
-    "img3" : "Screenshot.png",
+    "img1" : "./TribeVibe/Assets/socieites/Rotract/Rotract-1.jpeg",
+    "img2" : "./TribeVibe/Assets/socieites/Rotract/Rotract-2.jpeg",
+    "img3" : "./TribeVibe/Assets/socieites/Rotract/Rotract-3.jpeg",
+    "img4" : "./TribeVibe/Assets/socieites/Rotract/Rotract-4.jpeg",
     "desc" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pulvinar eros arcu, id aliquet lacus vulputate dapibus. Morbi felis massa, faucibus vel sapien non, vulputate convallis eros. Nunc mi arcu, fringilla at nulla eget."
 },
 {
