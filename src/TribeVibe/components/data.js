3e43a490ffebee5d8ff6e62f67e27a85924c4536
Madlad <72143774+Sahaj-Srivastava24@users.noreply.github.com>
2021-10-08 23:44:23 +0530

    data.js fixed

--- src/TribeVibe/components/data.js
+++ src/TribeVibe/components/data.js
@@ -1,7 +1,7 @@
 const society = [{
     key:1,
     "name" : "MLSC",
-    "img" : ["./TribeVibe/Assets/socieites/mlsc/mlsc-1.jpg", "./TribeVibe/Assets/socieites/mlsc/mlsc-2.jpg", "./TribeVibe/Assets/socieites/mlsc/mlsc-3.jpg"]
+    "img" : ["./TribeVibe/Assets/socieites/mlsc/mlsc-1.jpg", "./TribeVibe/Assets/socieites/mlsc/mlsc-2.jpg", "./TribeVibe/Assets/socieites/mlsc/mlsc-3.jpg"],
 //     "img2" : 
 //     "img3" : "./TribeVibe/Assets/socieites/mlsc/mlsc-3.jpg",
     "desc" : "Microsoft Learn Student Chapter is a technical society devoted to elevating the coding culture of Thapar Institute of Engineering and Technology by mentoring and motivating its students to refine their critical thinking, logical reasoning, and coding skills. MLSC was formed with the motive of providing the students a dynamic platform that serves as nodes of activity for not only its members but the computing community at large, offering all possible resources and opportunities to learn, master, and polish their abilities. We value holistic growth which includes injecting soft skills along with cultivating a  progressive mindset. MLSC effectuates it by organizing numerous technology-driven events including seminars, lectures, and the opportunity to meet peers and experts in many fields of interest, tech meetups, and Hackathons, and promoting team building. In the long run, MLSC plans to foster nurture young minds of TIET to a magnitude where they are proficient enough to compete amongst the nonpareil, thus upheaving the name of TIET."
@@ -9,7 +9,7 @@ const society = [{
 {
     key:3,
     "name" : "Mudra",
-    "img" : ["./TribeVibe/Assets/socieites/mudra/mudra-1.jpg", "./TribeVibe/Assets/socieites/mudra/mudra-2.png", "./TribeVibe/Assets/socieites/mudra/mudra-3.png",]
+    "img" : ["./TribeVibe/Assets/socieites/mudra/mudra-1.jpg", "./TribeVibe/Assets/socieites/mudra/mudra-2.png", "./TribeVibe/Assets/socieites/mudra/mudra-3.png",],
 //     "img2" : "./TribeVibe/Assets/socieites/mudra/mudra-2.png",
 //     "img3" : "./TribeVibe/Assets/socieites/mudra/mudra-3.png",
     "desc" : "Hey Juniors, Get ready for the most exciting event of Tribe Vibe. Mudra Society invites you to Razzle Dazzle, a fun-filled event with loads of games and many rewards. We indeed have something in store for all of you. Our team knows you all are stressed about your college life. Thus, MUDRA remains true to its vision and comes to rescue you out of tedious coding and studies. "
@@ -17,7 +17,7 @@ const society = [{
 {
     key:4,
     "name" : "FAPS",
-    "img" : ["./TribeVibe/Assets/socieites/faps/faps-1.jpg", "./TribeVibe/Assets/socieites/faps/faps-2.jpg", "./TribeVibe/Assets/socieites/faps/faps-3.jpg", "./TribeVibe/Assets/socieites/faps/faps-4.jpg", "./TribeVibe/Assets/socieites/faps/faps-5.jpg", "./TribeVibe/Assets/socieites/faps/faps-6.jpg", "./TribeVibe/Assets/socieites/faps/faps-7.jpg"]
+    "img" : ["./TribeVibe/Assets/socieites/faps/faps-1.jpg", "./TribeVibe/Assets/socieites/faps/faps-2.jpg", "./TribeVibe/Assets/socieites/faps/faps-3.jpg", "./TribeVibe/Assets/socieites/faps/faps-4.jpg", "./TribeVibe/Assets/socieites/faps/faps-5.jpg", "./TribeVibe/Assets/socieites/faps/faps-6.jpg", "./TribeVibe/Assets/socieites/faps/faps-7.jpg"],
 //     "img2" : "./TribeVibe/Assets/socieites/faps/faps-2.jpg",
 //     "img3" : "./TribeVibe/Assets/socieites/faps/faps-3.jpg",
 //     "img4" : "./TribeVibe/Assets/socieites/faps/faps-4.jpg",
@@ -27,11 +27,11 @@ const society = [{
 {
     key:5,
     "name" : "Rotract",
-    "img1" : ["./TribeVibe/Assets/socieites/Rotract/Rotract-1.jpeg", "./TribeVibe/Assets/socieites/Rotract/Rotract-2.jpeg", "./TribeVibe/Assets/socieites/Rotract/Rotract-3.jpeg", "./TribeVibe/Assets/socieites/Rotract/Rotract-4.jpeg",]
+    "img1" : ["./TribeVibe/Assets/socieites/Rotract/Rotract-1.jpeg", "./TribeVibe/Assets/socieites/Rotract/Rotract-2.jpeg", "./TribeVibe/Assets/socieites/Rotract/Rotract-3.jpeg", "./TribeVibe/Assets/socieites/Rotract/Rotract-4.jpeg",],
 //     "img2" : "./TribeVibe/Assets/socieites/Rotract/Rotract-2.jpeg",
 //     "img3" : "./TribeVibe/Assets/socieites/Rotract/Rotract-3.jpeg",
 //     "img4" : "./TribeVibe/Assets/socieites/Rotract/Rotract-4.jpeg",
-    "desc" : "Rotaract as an organisation currently function in more than 180+ countries originally began as a Rotary International youth program in 1968 at Charlotte North Rotary Club in Charlotte, North Carolina, United States, and has grown into a major organization of over 10,904 clubs spread around the world and over 203,000 members and are serving the society with the motto, Self Development | Fellowship through Service.Here at Rotaract you don't only get the chance to become an exemplary leader but also an emotionally intellectual human being who takes on world problems with utmost care and precision to benefit society in the end."
+    "desc" : "Rotaract as an organisation currently function in more than 180+ countries originally began as a Rotary International youth program in 1968 at Charlotte North Rotary Club in Charlotte, North Carolina, United States, and has grown into a major organization of over 10,904 clubs spread around the world and over 203,000 members and are serving the society with the motto, Self Development | Fellowship through Service.Here at Rotaract you don't only get the chance to become an exemplary leader but also an emotionally intellectual human being who takes on world problems with utmost care and precision to benefit society in the end.",
 }]
 
 export default society;
